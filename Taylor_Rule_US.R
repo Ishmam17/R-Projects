e07a2b378413ecfecc26e6d38be38ff34c007e7c
Ishmam17 <37196859+Ishmam17@users.noreply.github.com>
2024-11-05 23:38:26 -0500

    Add files via upload

---
+++ Taylor_Rule_US.R
@@ -0,0 +1,50 @@
+#------------------US Taylor Rule Model----------------------------------
+
+library(fredr)
+library(ggplot2)
+library(taskscheduleR)
+library(dplyr)
+library(tidyr)
+library(mFilter)
+library(quantmod)
+library(readr)
+
+
+setwd("C:/Users/ishma/Downloads/RUpdates")
+rm(list = ls())
+
+data <- read_csv("dataset.csv")
+cpi <-  data$CPIAUCSL_PC1
+ffr <- data$FEDFUNDS
+exp_inf <- data$EXPINF1YR
+rgdp <- data$GDPC1
+
+# Data transformations
+l_r_gdp <- log(rgdp)  # Log of GDP
+realgdp_hp <- hpfilter(rgdp, freq = 1600)
+l_realgdp_trend <- log(realgdp_hp$trend)  # Log of Trend GDP
+
+real_policy_rate = ffr - exp_inf
+real_nominal_rate_hp <- hpfilter(real_policy_rate, freq = 1600)
+real_nominal_rate_trend <- real_nominal_rate_hp$trend  # Log of Trend GDP
+
+ffr_lagged <- stats::lag(ffr, k = -1) 
+
+#Parameters and variables
+g1 = 0.8
+g2 = 3
+g3 = 1
+inf_target = 2.4
+nom_rate = real_nominal_rate_trend+exp_inf
+inf_gap = exp_inf - inf_target           
+out_gap = l_r_gdp - l_realgdp_trend
+
+#Taylor Rule
+
+tr_rate <- (g1 * ffr_lagged) + (1 - g1) * ((real_nominal_rate_trend + exp_inf + g2 * (inf_gap) + g3 * (out_gap)))
+
+tail(tr_rate)
+
+
+
+
